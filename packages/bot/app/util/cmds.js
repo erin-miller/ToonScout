6172f87b0c1ee88d1214e7b03808f07cdc08deaa
erin.miller <erinm1018@gmail.com>
2025-08-19 12:55:43 -0500

    sanitize: check for control characters

--- packages/bot/app/util/cmds.js
+++ packages/bot/app/util/cmds.js
@@ -9,3 +9,9 @@ export function getToonRendition(local_toon, pose) {
   const dna = local_toon.toon.style;
   return `https://rendition.toontownrewritten.com/render/${dna}/${pose}/1024x1024.png`;
 }
+
+export function sanitize(data) {
+  return data
+    .replace(/\\u[0-9a-fA-F]{4}/g, "") // Remove Unicode escape sequences
+    .replace(/[\u0000-\u001F\u007F�]/g, ""); // Remove control characters
+}
