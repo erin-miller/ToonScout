51e17c6d8c5c2f8c8f257d580d20d917091cfa7a
Erin Miller <erinm1018@gmail.com>
2025-09-19 14:22:54 -0500

    webapp(cavalcade): add timestamp support

--- packages/webapp/app/components/Home/EventBanner.tsx
+++ packages/webapp/app/components/Home/EventBanner.tsx
@@ -1,47 +1,40 @@
 import Banner from "@/app/components/Banner";
-import { useState } from "react";
 
-const API_LINK = process.env.NEXT_PUBLIC_API_HTTP;
-
+const carnivalEnums = {
+  INACTIVE: "inactive", // Holiday not running
+  RECHARGING: "recharging", // Holiday is running, but the parade isn't scheduled/running
+  IN_TRANSIT: "in-transit", // Parade is scheduled but not running
+  ACTIVE: "active", // Parade is running
+};
 interface EventBannerProps {
   isOpen: boolean;
   onClose: () => void;
+  msg: string;
+  timestamp: string;
+  status: string;
 }
 
-const EventBanner: React.FC<EventBannerProps> = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+const EventBanner: React.FC<EventBannerProps> = ({
+  isOpen,
+  onClose,
+  msg,
+  timestamp,
+  status,
+}) => {
+  const localTime = new Date(parseInt(timestamp) * 1000).toLocaleTimeString(
+    [],
+    { hour: "numeric", minute: "2-digit", hour12: true }
+  );
 
-  const [msg, setMsg] = useState<string>("");
-
-  const getCavalcade = async () => {
-    try {
-      const response = await fetch(API_LINK + "/utility/get-cavalcade", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        onClose();
-        return;
-      }
-      const data = await response.json();
-      if (data.status == null || data.status == "inactive") {
-        onClose();
-      }
-
-      setMsg(data.message);
-    } catch (error) {
-      console.error("Failed to fetch cavalcade data:", error);
-      onClose();
-      return;
-    }
-  };
-
-  if (isOpen && !msg) {
-    getCavalcade();
+  if (status == carnivalEnums.RECHARGING) {
+    msg = msg + ` come back at ${localTime} to find out the next location!`;
   }
 
+  if (status == carnivalEnums.IN_TRANSIT) {
+    msg = msg + ` starting at ${localTime}!`;
+  }
+
+  if (!isOpen) return null;
   return (
     <Banner
       isOpen={isOpen}
