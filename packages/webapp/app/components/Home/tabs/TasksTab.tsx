3c3f96c3ec61486080bd608582907e13c9396559
warmpoptart <128424622+warmpoptart@users.noreply.github.com>
2026-01-10 22:03:05 -0600

    style(task-progress): fix width of progress bar in tasks

--- packages/webapp/app/components/Home/tabs/TasksTab.tsx
+++ packages/webapp/app/components/Home/tabs/TasksTab.tsx
@@ -433,7 +433,7 @@ const TasksTab: React.FC<TabProps> = ({ toon: toons }) => {
 
     if (match) {
       return (
-        <div className="task-progress relative z-5 overflow-hidden min-w-64">
+        <div className="task-progress relative z-5 overflow-hidden w-full">
           <div
             className="bg-emerald-700 absolute inset-0 opacity-30 z-0"
             style={{ width: `${progress}%` }}
